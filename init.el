ed01c2f59a341e45ec825c48f9deabb58f1cd9ee
Graham Marlow <graham@onesignal.com>
2023-11-02 11:03:27 -0700

    Cleanup crafted emacs initialization

--- init.el
+++ init.el
@@ -7,6 +7,8 @@
 
 ;;; Code:
 
+(require 'cl-lib)
+
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
 (when (and custom-file
 	   (file-exists-p custom-file))
@@ -25,41 +27,34 @@
 
 (add-hook 'emacs-startup-hook #'configure-fonts)
 
-;;;; Packages
+(cl-defun crafted-init (&key modules packages)
+  (dolist (m modules)
+    (require (intern (format "crafted-%s-packages" m)) nil :noerror))
+  (dolist (p packages)
+    (add-to-list 'package-selected-packages p))
+  (package-install-selected-packages :noconfirm)
+  (dolist (m modules)
+    (require (intern (format "crafted-%s-config" m)) nil :noerror)))
 
-(require 'org-tempo)
-(require 'crafted-completion-packages)
-(require 'crafted-evil-packages)
-(require 'crafted-ui-packages)
-(require 'crafted-ide-packages)
-(require 'crafted-org-packages)
-(require 'crafted-writing-packages)
-(add-to-list 'package-selected-packages 'ef-themes)
-(add-to-list 'package-selected-packages 'evil-escape)
-(add-to-list 'package-selected-packages 'magit)
-(add-to-list 'package-selected-packages 'prettier-js)
-(add-to-list 'package-selected-packages 'aggressive-indent)
-(add-to-list 'package-selected-packages 'package-lint-flymake)
-(add-to-list 'package-selected-packages 'exec-path-from-shell)
-(add-to-list 'package-selected-packages 'flymake-clippy)
+;;;; Packages and crafted emacs modules
 
-(package-install-selected-packages :noconfirm)
+(crafted-init
+ :modules (append
+           '(defaults completion evil ui ide org writing)
+           (when (osxp) '(osx)))
+ :packages '(ef-themes
+             evil-escape
+             magit
+             prettier-js
+             aggressive-indent
+             package-lint-flymake
+             exec-path-from-shell
+             flymake-clippy))
 
-;;;; Configuration
+;;;; Custom
 
 (customize-set-variable 'crafted-ui-display-line-numbers t)
 (customize-set-variable 'denote-directory "~/denote/")
-
-(require 'crafted-defaults-config)
-(require 'crafted-completion-config)
-(require 'crafted-evil-config)
-(require 'crafted-ui-config)
-(require 'crafted-ide-config)
-(require 'crafted-writing-config)
-(when (osxp) (require 'crafted-osx-config))
-(crafted-ide-configure-tree-sitter)
-
-;;;; Custom
 
 (setq inhibit-startup-screen t
       ring-bell-function #'ignore)
@@ -101,6 +96,20 @@
       evil-escape-inhibit-functions (list (lambda () (not (evil-insert-state-p)))))
 (evil-escape-mode)
 
+;; Denote/org
+(require 'org-tempo)
+
+(defun journal-create-weekly ()
+  "Find or create a weekly journal entry."
+  (interactive)
+  (require 'denote)
+  (let* ((title (concat "week-" (format-time-string "%G-%U" (current-time))))
+         (pattern (concat ".*--" title))
+         (matches (denote-directory-files-matching-regexp pattern)))
+    (if matches
+        (find-file (car matches))
+      (denote title '("journal" "weekly") 'org nil nil 'weekly))))
+
 (add-hook 'org-mode-hook (lambda () (electric-pair-local-mode -1)))
 (add-hook 'org-mode-hook #'auto-fill-mode)
 (add-hook 'lisp-data-mode-hook #'aggressive-indent-mode)
@@ -114,17 +123,6 @@
 (global-set-key (kbd "C-c r") #'query-replace)
 (global-set-key (kbd "C-c g") #'magit-status)
 (global-set-key (kbd "C-c n j") #'journal-create-weekly)
-
-(defun journal-create-weekly ()
-  "Find or create a weekly journal entry."
-  (interactive)
-  (require 'denote)
-  (let* ((title (concat "week-" (format-time-string "%G-%U" (current-time))))
-         (pattern (concat ".*--" title))
-         (matches (denote-directory-files-matching-regexp pattern)))
-    (if matches
-        (find-file (car matches))
-      (denote title '("journal" "weekly") 'org nil nil 'weekly))))
 
 (defun kill-other-buffers ()
   "Kill all non-special buffers other than the current."
